347b964b8f8d4bad56884d5ca9d03c5328b2459b
umesh kadam <umeshk149@gmail.com>
2021-12-20 15:02:24 +0530

    mongodb works - registration

--- express/index.js
+++ express/index.js
@@ -1,15 +1,29 @@
-const {json} = require('express');
-let express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+// app
 const app = express();
 const port = 3001;
 
-// routes
-let authRoute = require('./routes/auth');
-app.use('/api/user', authRoute);
+// env
+dotenv.config();
+
+// mongodb
+mongoose.connect(
+  process.env.CONNECT_DB,
+  {
+    useNewUrlParser: true,
+  },
+  () => console.log('connected with MongoDB')
+);
 
 // middlewares
 app.use(express.json());
+
+// middleware - routes
+let authRoute = require('./routes/auth');
+app.use('/api/user', authRoute);
 
 // listen
 app.listen(port, () => {
--- express/routes/auth.js
+++ express/routes/auth.js
@@ -1,10 +1,21 @@
-let router = require('express').Router();
+const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/login', (req, res) => {
-  res.send('login user');
-});
-router.post('/register', (req, res) => {
-  res.send('register user');
+router.post('/login', (req, res) => {});
+
+router.post('/register', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
